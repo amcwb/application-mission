ac43813ab5880a2f091fbdea25fc7c23a276a509
Avery <19372006+starsflower@users.noreply.github.com>
2021-08-23 17:56:23 +0100

    Use envvars when possible

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 import os
 
-SECRET_KEY = os.urandom(16)
-SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-JWT_SECRET = os.urandom(16)
+SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(16))
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:")
+JWT_SECRET = os.getenv("JWT_SECRET", os.urandom(16))
