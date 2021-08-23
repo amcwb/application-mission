0b5a53f8c2cbcefca5f0965b15eb02a9011f0dae
Avery <19372006+starsflower@users.noreply.github.com>
2021-08-23 17:55:44 +0100

    Set data for tasks

--- backend/views/tasks.py
+++ backend/views/tasks.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+import json
 from flask.json import jsonify
 from backend import db
 from backend.authenticate import is_authenticated
-from backend.models.usertask import Task
+from backend.models.usertask import Task, User
 from flask import Blueprint, current_app, render_template, request, session
 
 tasks = Blueprint("tasks", __name__)
@@ -44,7 +46,6 @@ def delete():
 @tasks.route("/info", methods=["GET"])
 def info():
     task_id = request.form.get("task_id")
-
     task = Task.query.filter_by(id=task_id).first()
     if task:
         return jsonify({
@@ -54,5 +55,58 @@ def info():
             "author_id": task.author_id,
             "assigned_users": list(map(lambda p: p.id, task.assigned_users))
         }), 200
+    else:
+        return jsonify({}), 404
+
+@is_authenticated
+@tasks.route("/list", methods=["GET"])
+def list_():
+    tasks = Task.query.all()
+    data_tasks = []
+    for task in tasks:
+        data_tasks.append({
+            "task_id": int(task.id),
+            "due": task.due,
+            "content": task.content,
+            "author_id": task.author_id,
+            "assigned_users": list(map(lambda p: p.id, task.assigned_users))
+        })
+    
+    return jsonify({"tasks": data_tasks}), 200
+
+
+def _get_datetime_or_none(*options):
+    for option in options:
+        try:
+            if isinstance(option, datetime):
+                return option
+            return datetime.fromtimestamp(option)
+        except TypeError:
+            pass
+    
+    return None
+
+
+@is_authenticated
+@tasks.route("/set_data", methods=["POST"])
+def set_data():
+    task_id = request.form.get("task_id")
+    task = Task.query.filter_by(id=task_id).first()
+    if task:
+        try:
+            data = json.loads(request.form.get("data"))
+        except ValueError:
+            return jsonify({
+                "error": "Invalid data"
+            }), 400
+        
+        task.due = _get_datetime_or_none(data.get("due"), task.due)
+        task.content = data.get("content", task.content)
+        assigned_users = data.get("assigned_users", map(lambda p: p.id, task.assigned_users))
+        task.assigned_users = list(map(lambda p: User.query.filter_by(id=p).first(), assigned_users))
+
+        db.session.commit()
+
+        return jsonify({}), 200
     else:
         return jsonify({}), 404
